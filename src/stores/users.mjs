62ef9c615f89b2b9dff186830b7133c1de1c0cb5
Nuszpl Botond <nuszplbotond@gmail.com>
2023-01-10 21:59:25 +0100

    Revert "Added typescript"
    This reverts commit 0e3ef59581a030ef8129d5dc285a36088183f8c0.

--- src/stores/users.mjs
+++ src/stores/users.mjs
@@ -1,7 +1,6 @@
-import { User } from 'src/models'
 import uuid4 from 'uuid4'
 
-const userDatabase: User[] = [
+const userDatabase = [
   { id: '1', name: 'User1', age: 21 },
   { id: '2', name: 'User2', age: 22 },
   { id: '3', name: 'User3', age: 23 },
@@ -12,11 +11,11 @@ const userStore = {
     return userDatabase
   },
 
-  getUser: async (userId: string) => {
+  getUser: async (userId) => {
     return userDatabase.find((user) => user.id === userId)
   },
 
-  addUser: async (user: Omit<User, 'id'>) => {
+  addUser: async (user) => {
     const newUser = { ...user, id: uuid4() }
     userDatabase.push(newUser)
     return newUser
