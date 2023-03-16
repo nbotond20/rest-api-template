a7fbbfab87577cf7bbe2792601db53224ed03b86
Nuszpl Botond <nuszplbotond@gmail.com>
2023-03-16 22:05:05 +0100

    Added prisma

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -4,9 +4,16 @@ import { NextFunction, Request, Response } from 'express'
 // Custom error handler that returns a JSON object with the error message
 const errorHandler = (err: BoomError, _req: Request, res: Response, next: NextFunction) => {
   const boomError: BoomError = Boom.isBoom(err) ? err : Boom.boomify(err)
-  res
-    .status(boomError.output.statusCode)
-    .json({ status: boomError.output.statusCode, message: Boom.isBoom(err) ? err.message : 'Internal server error' })
+  res.status(boomError.output.statusCode).json({
+    status: boomError.output.statusCode,
+    message: Boom.isBoom(err) ? err.message : 'Internal server error',
+    ...(Boom.isBoom(err) &&
+      err.data && {
+        data: {
+          ...boomError.data,
+        },
+      }),
+  })
   next()
 }
 
