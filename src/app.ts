0f29b48a648cb82803459a09bd957e318373986d
Nuszpl Botond <nuszplbotond@gmail.com>
2023-03-16 23:17:45 +0100

    Added rate limiter

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
 import * as dotenv from 'dotenv'
+import { limiter } from './utils/rateLimiter'
 
 // Load environment variables from .env file
 dotenv.config()
@@ -15,5 +16,6 @@ app.use(cors())
 app.use(helmet())
 app.use(express.json())
 app.use(morgan('combined'))
+app.use(limiter)
 
 export default app
